d72ebc281947d96e985c376d4f142ec7affc02e3
toleAndresDep <andres__toledo@hotmail.com>
2024-05-11 12:50:40 -0300

    prueba Vendido

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     buttons.forEach(function(button) {
         button.addEventListener('click', function() {
             var phoneNumber = '542235313045'; // Reemplaza con tu número de WhatsApp
-            var message = encodeURIComponent('Hola, me gustaría comprar este producto.'); // Puedes personalizar el mensaje si lo deseas
+            var message = encodeURIComponent('Hola Andres, me gustaría comprar este producto.'); // Puedes personalizar el mensaje si lo deseas
             var url = 'https://wa.me/' + phoneNumber + '?text=' + message;
             
             window.open(url, '_blank');
